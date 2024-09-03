78ef0ba231df05534dd056402211221a54386c24
HuynhNhuNgoc1006 <ngocfhfh12@gmail.com>
2024-09-03 21:15:48 +0700

    bai11.py

--- bai11.py
+++ bai11.py
@@ -1,37 +1,5 @@
-import random
+ky_tu_thuong = input("Nhập một ký tự chữ thường: ")
 
-def xin_so_ngau_nhien(a, b):
-  """Xin một số ngẫu nhiên trong khoảng từ a đến b (bao gồm cả a và b)
+ky_tu_hoa = ky_tu_thuong.upper()
 
-  Args:
-    a: Giới hạn dưới
-    b: Giới hạn trên
-
-  Returns:
-    Một số ngẫu nhiên trong khoảng đã cho
-  """
-
-  return random.randint(a, b)
-
-# Nhập lựa chọn
-lua_chon = input("Chọn khoảng bạn muốn: \n "
-                 "a. 0 đến 100\n"
-                 "b. 50 đến 99\n"
-                 "c. -39 đến 79\n"
-                 "d. -79 đến -39\n"
-                 "Khoản bạn muốn chọn là:")
-
-# Xin số ngẫu nhiên theo lựa chọn
-if lua_chon == 'a':
-  so_ngau_nhien = xin_so_ngau_nhien(0, 100)
-elif lua_chon == 'b':
-  so_ngau_nhien = xin_so_ngau_nhien(50, 99)
-elif lua_chon == 'c':
-  so_ngau_nhien = xin_so_ngau_nhien(-39, 79)
-elif lua_chon == 'd':
-  so_ngau_nhien = xin_so_ngau_nhien(-79, -39)
-else:
-  print("Lựa chọn không hợp lệ!")
-  exit()
-print("Số ngẫu nhiên:", so_ngau_nhien)
-
+print("Ký tự chữ hoa tương ứng là:", ky_tu_hoa)
